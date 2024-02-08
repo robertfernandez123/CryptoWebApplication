3ba9e4c898225031b46c34428b67c8f91c9895d9
robertfernandez123 <support@fca-org-uk.io>
2024-02-08 22:28:10 +0100

    first commit

---
+++ src/main/java/crypto/web/CryptoWebApplication/CryptoWebApplication.java
@@ -0,0 +1,14 @@
+package crypto.web.CryptoWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoWebApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/crypto/web/CryptoWebApplication/CryptoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package crypto.web.CryptoWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
